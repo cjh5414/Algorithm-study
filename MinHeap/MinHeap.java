a4154c04b322966dfc7babd12f5059f993b364b1
cjh5414 <cjh5414@gmail.com>
2018-08-26 10:52:20 -0700

    Add MinHeap insert

--- MinHeap/MinHeap.java
+++ MinHeap/MinHeap.java
@@ -5,9 +5,22 @@ class MinHeap {
     int[] heap = new int[256];
 
     public void insert(int val) {
+        int tmpIdx = this.idx;
+        while (tmpIdx != 1 && heap[tmpIdx] < heap[tmpIdx / 2]) {
+            tmpIdx = tmpIdx / 2;
+        }
 
+        heap[idx] = heap[tmpIdx];
+        heap[tmpIdx] = val;
+        idx++;
     }
 
-    public int delete();
-    public void print();
+    public int delete() {
+        return 0;
+    }
+
+
+    public void print() {
+        return;
+    }
 }
