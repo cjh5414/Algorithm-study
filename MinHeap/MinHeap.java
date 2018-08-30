dd2c066c2b90237a68baddffbf47f00a4d93ed35
cjh5414 <cjh5414@gmail.com>
2018-08-29 17:47:34 -0700

    Correct MinHeap Delete method

--- MinHeap/MinHeap.java
+++ MinHeap/MinHeap.java
@@ -17,30 +17,27 @@ class MinHeap {
     }
 
     public int delete() {
-        int val = heap[1];
+        int minVal = heap[1];
         int tmpIdx = 1, minIdx;
+        int val = heap[(idx--)-1];
 
-        heap[1] = heap[idx-1];
-
-        while (tmpIdx * 2 + 1 < idx) {
-            if (tmpIdx * 2 + 1 == idx)
+        while (tmpIdx * 2 < idx) {
+            if (tmpIdx * 2 == idx - 1)
                 minIdx = tmpIdx * 2;
             else
                 minIdx = heap[tmpIdx * 2] < heap[tmpIdx * 2 + 1] ? tmpIdx * 2 : tmpIdx * 2 + 1;
 
-            if (heap[minIdx] < heap[tmpIdx]) {
-                int tmp = heap[tmpIdx];
+            if (val > heap[minIdx]) {
                 heap[tmpIdx] = heap[minIdx];
-                heap[minIdx] = tmp;
                 tmpIdx = minIdx;
             }
 
             else break;
         }
 
-        idx--;
+        heap[tmpIdx] = val;
 
-        return val;
+        return minVal;
     }
 
 
